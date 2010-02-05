098fcd46f4b4ed82752cd09fb0c8767371cd645d
Brian Finney <bri+git@nfinney.com>
2010-02-05 21:36:01 +0000

    base rails-3 app

---
+++ Gemfile
@@ -0,0 +1,22 @@
+# Edit this Gemfile to bundle your application's dependencies.
+source 'http://gemcutter.org'
+source 'http://gems.github.com'
+
+
+gem "rails", "3.0.0.beta"
+
+## Bundle edge rails:
+# gem "rails", :git => "git://github.com/rails/rails.git"
+
+
+## Bundle the gems you use:
+# gem "bj"
+# gem "hpricot", "0.6"
+# gem "sqlite3-ruby", :require => "sqlite3"
+gem "aws-s3", :require => "aws/s3"
+
+## Bundle gems used only in certain environments:
+group :test do
+  gem "rspec"
+  gem "webrat"
+end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '7df8b7b50287bd12f06b4319c19a93e87aea237f9ba0273c8750db885c784b7c34af00eeaf549a644befd7e8438e0629f24ebbc77ad4ba403a391a2562aeabe6'
